914a45684f5edafa99314f6fa5da92787448afff
Harun Bekcan <harunbekcan@gmail.com>
2022-09-13 10:06:57 +0300

    screenRoutes completed

--- app/src/main/java/com/harunbekcan/jetpackcomposenavigationcomponent/navigation/ScreenRoutes.kt
+++ app/src/main/java/com/harunbekcan/jetpackcomposenavigationcomponent/navigation/ScreenRoutes.kt
@@ -1,4 +1,13 @@
 package com.harunbekcan.jetpackcomposenavigationcomponent.navigation
 
-class ScreenRoutes {
+import com.harunbekcan.jetpackcomposenavigationcomponent.utils.Constants.FAVORITES_SCREEN
+import com.harunbekcan.jetpackcomposenavigationcomponent.utils.Constants.HOME_SCREEN
+import com.harunbekcan.jetpackcomposenavigationcomponent.utils.Constants.NOTIFICATIONS_SCREEN
+import com.harunbekcan.jetpackcomposenavigationcomponent.utils.Constants.PROFILE_SCREEN
+
+sealed class ScreenRoutes(route : String) {
+    object HomeScreen : ScreenRoutes(HOME_SCREEN)
+    object FavoritesScreen : ScreenRoutes(FAVORITES_SCREEN)
+    object NotificationsScreen : ScreenRoutes(NOTIFICATIONS_SCREEN)
+    object ProfileScreen : ScreenRoutes(PROFILE_SCREEN)
 }
---
+++ app/src/main/java/com/harunbekcan/jetpackcomposenavigationcomponent/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.harunbekcan.jetpackcomposenavigationcomponent.utils
+
+object Constants {
+    const val HOME_SCREEN = "HOME_SCREEN"
+    const val FAVORITES_SCREEN = "FAVORITES_SCREEN"
+    const val NOTIFICATIONS_SCREEN = "NOTIFICATIONS_SCREEN"
+    const val PROFILE_SCREEN = "PROFILE_SCREEN"
+}
